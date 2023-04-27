4bc028ba3e401b2f5707f2fca66837ac76357cca
Deysi Morales Magadan <77804551+deysim@users.noreply.github.com>
2023-04-27 19:29:43 -0400

    a03

---
+++ bin/rps-cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import { rps } from "../lib/rpsls.js"
+
+var args = minimist(process.argv.slice(2));
+
+function rule() {
+    console.log(
+`Rules for Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock CRUSHES Scissors`);
+    process.exit(0); 
+}
+
+function help () {
+	    console.log(
+`Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);    
+    process.exit(0); 
+}
+
+
+if (args.r || args.rules) {rule();}
+if (args.h || args.help) {help();}
+
+
+try {
+	console.log(JSON.stringify(rps(args._[0])));
+} catch (error) {
+	if (error instanceof RangeError) {
+		help();
+		rule();
+		process.exit(1);
+	}
+}
---
+++ bin/rpsls-cli.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import { rpsls } from "../lib/rpsls.js"
+
+var args = minimist(process.argv.slice(2));
+
+function help() {
+	console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+    process.exit(0);
+}
+
+function rule() {
+	console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+
+if (args.r || args.rules) {rule();}
+if (args.h || args.help) {help();}
+
+try {
+	console.log(JSON.stringify(rpsls(args._[0])));
+} catch (error) {
+	if (error instanceof RangeError) {
+		help();
+		rule();
+		process.exit(1);
+	}
+}
